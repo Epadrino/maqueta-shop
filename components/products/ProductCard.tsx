e8457b60a427358754963917873852d9de82642a
Epadrino <epadrinoa@gmail.com>
2023-02-11 10:34:20 -0400

    commit
    -Cambio de imagen al pasar el cursor en la ProducCard
    -Se agregaron nuevos componentes a la ProducCard

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,5 +1,13 @@
-import React, { FC } from 'react';
-import { Card, CardActionArea, CardMedia, Grid } from '@mui/material';
+import React, { FC, useMemo, useState } from 'react';
+
+import {
+	Box,
+	Card,
+	CardActionArea,
+	CardMedia,
+	Grid,
+	Typography,
+} from '@mui/material';
 
 import { IProduct } from '../../interfaces';
 
@@ -8,23 +16,42 @@ interface Props {
 }
 
 export const ProductCard: FC<Props> = ({ product }) => {
+	// hook para estado de mouse en la card
+	const [isHovered, setIsHovered] = useState(false);
+
+	//const para Cambiar Imagenes
+	const productImage = useMemo(() => {
+		return isHovered
+			? `products/${product.images[1]}`
+			: `products/${product.images[0]}`;
+	}, [isHovered, product.images]);
+
 	return (
 		<Grid
 			item
 			xs={6}
 			sm={4}
-			key={product.slug}
+			onMouseEnter={() => setIsHovered(true)}
+			onMouseLeave={() => setIsHovered(false)}
 		>
 			{' '}
 			<Card>
 				<CardActionArea>
 					<CardMedia
 						component="img"
-						image={`products/${product.images[0]}`}
+						className="fadeIn"
+						image={productImage}
 						alt={product.title}
 					/>
 				</CardActionArea>
 			</Card>
+			<Box
+				sx={{ mt: 1 }}
+				className="fadeIn"
+			>
+				<Typography fontWeight={700}>{product.title}</Typography>
+				<Typography fontWeight={500}>${product.price}</Typography>
+			</Box>
 		</Grid>
 	);
 };
